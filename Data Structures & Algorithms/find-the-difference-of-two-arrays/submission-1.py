f85121aa37f0be7863263aeee4b93c8a549d1f28
quentinRevillon <82468278+quentinRevillon@users.noreply.github.com>
2026-08-25 15:32:18 +0200

    Add: find-the-difference-of-two-arrays - submission-1

---
+++ Data Structures & Algorithms/find-the-difference-of-two-arrays/submission-1.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        answer=[[],[]]
+        answer[0], answer[1]=list(set(nums1)-set(nums2)), list(set(nums2)-set(nums1))
+        return answer
